98816a910f8d532e033bc04dae570b3db0b871e4
Roshini <144936385+Roshini1505@users.noreply.github.com>
2024-07-20 15:11:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,34 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const customerRoutes = require("./routes/customers");
 
+// load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middleware
 app.use(bodyParser.json());
 
+// connect to mongodB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
+
+// error message
 db.on("error", console.error.bind(console, "connection error:"));
+
+// success message
 db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
 app.use("/api/customers", customerRoutes);
 
+// start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
